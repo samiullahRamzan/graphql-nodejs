cb261a9e601c87c6dfa912787cfe72ad135f434b
samiullah-dev <samidev336@gmail.com>
2026-03-07 13:22:08 +0500

    updated

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const {ApolloServer}= require('@apollo/server')
+const {expressMiddleware}= require('@apollo/server/express4')
+
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
+
+async function startServer(params) {
+    const app=express();
+    const server=new ApolloServer({
+      typeDefs:`
+      
+         type Todo{
+             id:ID!
+             title: String!
+             completed: Boolean
+         }
+
+         type Query{
+
+         getTodos:[Todo]
+         }
+      `
+    })
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start()
+    app.use('/graphql',expressMiddleware(server));
+
+    app.listen(8000,()=> console.log('Server started at port 8000'))
+
+}
+
+startServer();
